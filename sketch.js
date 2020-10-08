c745d90fe79a1e347ee2acc14859b2643bbaded0
DivyeshSaraf1217 <71771555+DivyeshSaraf1217@users.noreply.github.com>
2020-10-08 11:45:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.30, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.50, isStatic:true});
 	World.add(world, packageBody);
 	
 
